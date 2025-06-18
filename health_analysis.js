c49856f7f7229ed9282f5e2f07403d68f158a2e8
Shashwatt699 <shashwatsaxena4545@gmail.com>
2025-06-18 01:38:35 -0400

    lot done

---
+++ health_analysis.js
@@ -0,0 +1,54 @@
+const addPatientButton = document.getElementById("addPatient");
+const report = document.getElementById("report");
+const btnSearch = document.getElementById("btnSearch");
+const patients = [];
+
+function addPatient() {
+    const name = document.getElementById("name").value;
+    const gender = document.querySelector('input[name="gender"]:checked');
+    const age = document.getElementById("age").value;
+    const condition = document.getElementById("condition").value;
+    
+    if (name && gender && age && condition) {
+        patients.push({
+            name, gender: gender.value, age, condition
+        });
+        resetForm();
+        generateReport();
+    }
+
+}
+
+function resetForm(){
+    document.getElementById("name").value = "";
+    document.querySelector('input[name="gender"]:checked').checked = false;
+    document.getElementById("age").value = "";
+    document.getElementById("condition").value = "";
+}
+
+function generateReport(){
+    const numPatients = patients.length;
+    const conditionsCount = {
+        Diabetes: 0,
+        Thyroid: 0,
+        "High Blood Pressure": 0,
+    };
+
+    const genderConditionsCount = {
+        Male: {
+            Diabetes: 0,
+            Thyroid: 0,
+            "High Blood Pressure": 0,
+        },
+        Female: {
+            Diabetes: 0,
+            Thyroid: 0,
+            "High Blood Pressure": 0,
+        },
+    };
+
+    for (const patient of patients) {
+        conditionsCount[patient.condition]++;
+        genderConditions
+    }
+}
